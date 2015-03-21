fb70a09efe72bdd3a7f1078735b3583f5dfa2c01
Iuri Diniz <iuridiniz@gmail.com>
2015-03-21 14:37:38 -0300

    allowing timeout

--- app/src/main/java/tv/supermidia/site/Util.java
+++ app/src/main/java/tv/supermidia/site/Util.java
@@ -5,13 +5,18 @@ import android.util.Log;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
 import java.net.URL;
 
 /**
  * Created by iuri on 12/01/15.
  */
 public class Util {
+
     public static boolean checkURL(String urlString) {
+        return checkURL(urlString, 0);
+    }
+    public static boolean checkURL(String urlString, int timeoutMili) {
         URL u = null;
         try {
             u = new URL(urlString);
@@ -23,8 +28,12 @@ public class Util {
         try {
             huc = (HttpURLConnection)  u.openConnection();
             huc.setRequestMethod("GET");
+            huc.setConnectTimeout(timeoutMili);
             huc.connect();
             return huc.getResponseCode() == HttpURLConnection.HTTP_OK;
+        } catch (SocketTimeoutException e) {
+            Log.e("UTIL::checkURL", "SocketTimeoutException: " + urlString);
+            return false;
         } catch (IOException e) {
             Log.e("UTIL::checkURL", "IOException: " + urlString);
             return false;
